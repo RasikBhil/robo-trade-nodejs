4a711da91001b7c03f8536d780b965ce314e7d10
Rasik Bhil <rasik.bhil.mit@gmail.com>
2024-02-15 17:26:13 +0530

    db collection name change

--- src/controllers/instruments/services.js
+++ src/controllers/instruments/services.js
@@ -1,16 +1,18 @@
-import db from '../../datasource/index.js'
-const instruments = db.instrumentModel;
+import db from "../../datasource/index.js";
+const instrumentsDB = db.instrumentModel;
 
 export function addInstruments(instruments) {
-    return new Promise.all(instruments.map((instrument) => {
-        return new Promise(async (resolve, reject) => {
-            const instrumentModel = new instruments(instrument)
-            try {
-                await instrumentModel.save();
-                resolve({message:'instruments added'})
-            } catch (error) {
-                reject(error);
-            }
-        })
-    }))
+  return new Promise.all(
+    instruments.map((instrument) => {
+      return new Promise(async (resolve, reject) => {
+        const instrumentModel = new instrumentsDB(instrument);
+        try {
+          await instrumentModel.save();
+          resolve({ message: "instruments added" });
+        } catch (error) {
+          reject(error);
+        }
+      });
+    })
+  );
 }
